4462b8351488514430306e2b9e8b630689f418df
liyanfang <1136315144@qq.com>
2023-03-14 10:30:23 +0800

    feat: first commit

---
+++ bin/demo-cli.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+const program = require("commander");
+// const chalk = require("chalk");
+
+program
+  .command("create <app-name>")
+  .description("create a new project")
+  .option("-f, --force", "overwrite target directory if it exist")
+  .action((name, options) => {
+    console.log(name, options);
+  });
+// 解析用户命令参数的操作一定要在最后一行，否则什么都不会执行
+program.parse(process.argv);
