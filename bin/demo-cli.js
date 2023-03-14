0c9a9c6f2b967fdaa842b7b2d29eb6cf3e9b8844
liyanfang <1136315144@qq.com>
2023-03-14 19:15:24 +0800

    feat: 添加lib

--- bin/demo-cli.js
+++ bin/demo-cli.js
@@ -1,13 +1,14 @@
 #! /usr/bin/env node
 const program = require("commander");
-// const chalk = require("chalk");
+const chalk = require("chalk");
 
 program
   .command("create <app-name>")
-  .description("create a new project")
+  .description(chalk.cyan("create a new project"))
   .option("-f, --force", "overwrite target directory if it exist")
   .action((name, options) => {
-    console.log(name, options);
+    // console.log(name, options);
+    require("../lib/create")(name, options);
   });
 // 解析用户命令参数的操作一定要在最后一行，否则什么都不会执行
 program.parse(process.argv);
